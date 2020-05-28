700898ffc8b41b8e34c2694d041117d632c1e985
Steven <delchambre.steven@gmail.com>
2020-05-28 10:53:20 +0200

    Devis / collaborateur

---
+++ index_collaborateur.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Accueil</title>
+</head>
+<body>
+
+<?php
+
+require_once 'include/header.html';
+require_once 'include/navbar.html';
+echo '<div class="container">';
+require_once 'include/collaborateur.html';
+echo '</div>';
+require_once 'include/footer.html';
+
+?>
+
+<script src="assets/js/js.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
